de388cd1061b9f55f7199e0780e5a2af918ff6a6
Leandro Falcon <cronosteo1034@gmail.com>
2022-10-05 03:50:46 -0300

    text changer ready

--- text-changer.js
+++ text-changer.js
@@ -1,21 +1,20 @@
-const cards = document.querySelectorAll('.team_member');
-console.log(cards);
-const cardsFront = document.querySelectorAll('.card-front');
-const cardsBack = document.querySelectorAll('.card-back');
-console.log(cardsBack);
 
-cards.forEach(item => {
-    item.addEventListener('click', event => {
-        cardsFront.forEach(p1 => {
-            p1.classList.toggle("card-front-show");
-        });
-        cardsBack.forEach(p2 => {
-            p2.classList.toggle("card-back-show");
-        })
+// const cardsFront = document.querySelectorAll('.card-front');
+// const cardsBack = document.querySelectorAll('.card-back');
+// console.log(cardsBack);
+
+// cards.forEach(item => {
+//     item.addEventListener('click', event => {
+//         cardsFront.forEach(p1 => {
+//             p1.classList.toggle("card-front-show");
+//         });
+//         cardsBack.forEach(p2 => {
+//             p2.classList.toggle("card-back-show");
+//         })
         
         
-    })
-});
+//     })
+// });
 
 // card.forEach(item)addEventListener("click",function (event) {
 //         nabilaFront.classList.toggle("card-front");
@@ -23,3 +22,67 @@ cards.forEach(item => {
 //     },
 //     false
 // );
+document.getElementById("1").addEventListener(
+    "click",
+    function (event) {
+      document.getElementById("1front").classList.toggle("card-front-show");
+      document.getElementById("1back").classList.toggle("card-back-show");
+    },
+    false
+  );
+  document.getElementById("2").addEventListener(
+    "click",
+    function (event) {
+        document.getElementById("2front").classList.toggle("card-front-show");
+        document.getElementById("2back").classList.toggle("card-back-show");
+    },
+    false
+  );
+  document.getElementById("3").addEventListener(
+    "click",
+    function (event) {
+        document.getElementById("3front").classList.toggle("card-front-show");
+        document.getElementById("3back").classList.toggle("card-back-show");
+    },
+    false
+  );
+  document.getElementById("4").addEventListener(
+    "click",
+    function (event) {
+        document.getElementById("4front").classList.toggle("card-front-show");
+        document.getElementById("4back").classList.toggle("card-back-show");    
+    },
+    false
+  );
+  document.getElementById("5").addEventListener(
+    "click",
+    function (event) {
+        document.getElementById("5front").classList.toggle("card-front-show");
+        document.getElementById("5back").classList.toggle("card-back-show");    
+    },
+    false
+  );
+  document.getElementById("6").addEventListener(
+    "click",
+    function (event) {
+        document.getElementById("6front").classList.toggle("card-front-show");
+        document.getElementById("6back").classList.toggle("card-back-show");    
+    },
+    false
+  );
+  document.getElementById("7").addEventListener(
+    "click",
+    function (event) {
+        document.getElementById("7front").classList.toggle("card-front-show");
+        document.getElementById("7back").classList.toggle("card-back-show");    
+    },
+    false
+  );
+  document.getElementById("8").addEventListener(
+    "click",
+    function (event) {
+        document.getElementById("8front").classList.toggle("card-front-show");
+        document.getElementById("8back").classList.toggle("card-back-show");    
+    },
+    false
+  );
